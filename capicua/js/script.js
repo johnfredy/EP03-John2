55cd01e40e1ada0f2c2d9e666195abd459bba3d6
john fredy <jhoacosta93@hotmail.com>
2015-09-17 19:01:31 -0500

    capicua

--- capicua/js/script.js
+++ capicua/js/script.js
@@ -2,7 +2,7 @@ window.onload = function()
 {
     var sierpinski = false;
 
-    var trianguloPascal = function(niveles)
+    var escalaCapicua = function(niveles)
     {
         var pascal = [];
         var numero=1;
@@ -19,36 +19,27 @@ window.onload = function()
 
     };
 
-    //Imprimir el triángulo de Pascal...
-    var imprimeTriangulo = (function imprimeTriangulo(nivel)
+    var imprimeCapicua = (function imprimeCapicua(nivel)
     {
         var txt = "";
-        var capicua = trianguloPascal(nivel);
+        var capicua = escalaCapicua(nivel);
         var colorCelda = "";
         nom_div("pascal").innerHTML = "";
         for(var i = 0; i < capicua.length; i++)
         {
             txt += "<div align = 'center' class = 'nivel' style = 'padding-bottom: "+10+"px;'>"+capicua[i];
-            /*for(var c = 0; c < 1; c++)
-            {
-                txt += "<div style = 'display: inline-block; background-color: blue' class = 'celda'>" + 
-                            capicua[i][c] + 
-                        "</div>";
-            }
-            txt += "</div>";*/
+           
         }
         nom_div("pascal").innerHTML = txt;
-        return imprimeTriangulo;
+        return imprimeCapicua;
     })(2);
 
-    //Para cambiar el rango del triángulo...
     nom_div("rango").addEventListener('change', function(event)
     {
         nom_div("txtrango").innerHTML = this.value;
-        imprimeTriangulo(Number(this.value));
+        imprimeCapicua(Number(this.value));
     });
 
-    //Para imprir elementos en el HTML...
     function nom_div(div)
     {
         return document.getElementById(div);
